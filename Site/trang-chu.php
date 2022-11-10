f54952fef28367afe5c9aab244a7fdbe360cf005
nguyenphucquyet <114094880+nguyenphucquyet@users.noreply.github.com>
2022-11-10 21:03:58 +0700

    trang chu

--- Site/trang-chu.php
+++ Site/trang-chu.php
@@ -1,3 +1,39 @@
 <div class="container mx-auto">
-    <h1>Đây là Trang Chủ</h1>
+    <header>
+        <img src="../Asset/Image/logo/banner_x_t100 1.png" class="w-full" alt="">
+    </header>
+
+    <div class="grid grid-cols-3 mt-[40px]">
+        
+      <div>
+        <img src="../Asset/Image/logo/nen-dau-tu-mua-lens-hay-nang-cap-body-may-anh--1 1.png"  alt="">
+      </div>
+      <div>
+        <img src="../Asset/Image/logo/images (1) 1.png" alt="">
+      </div>
+      <div>
+        <img src="../Asset/Image/logo/nen-dau-tu-mua-lens-hay-nang-cap-body-may-anh--2 1.png" alt="">
+      </div>
+    </div>
+
+    <div>
+        <h2 class="mt-[15px] font-medium text-[23px]">Sản phẩm bán chạy</h2>
+    </div>
+
+    <div class="grid grid-cols-4">
+    <div class="border border-[#EFA969]">
+        <div>
+           <img src="../Asset/Image/product/canon-eos-1500d-kit-1855mm-f3556-iii(2) 1.png" alt=""> 
+        </div>
+        <div>
+            <p>MÁY ẢNH CANON EOS 1500D KIT EF-S18-55MM F3.5-5.6 IS II</p>
+        </div>
+        <div>
+            <span>11,090,000 đ</span>
+        </div>
+        <div>
+            <button>Mua hàng</button>
+        </div>
+    </div>
+    </div>
 </div>

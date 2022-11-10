2ccc78135dc9debeb2ec98343d69565e4d6c7d58
TriChua18 <trintph20275@fpt.edu.vn>
2022-11-10 16:51:57 +0700

    Form đăng nhập

--- Site/dang-nhap.php
+++ Site/dang-nhap.php
@@ -1,3 +1,34 @@
 <div class="container mx-auto">
-    <h1>Đây là Trang đăng nhập</h1>
+    <div class="flex justify-center mt-[28px]">
+        <form action="" class="w-[600px] flex justify-center mb-[190px]" method="POST">
+            <div>
+                <div class="flex justify-center">
+                    <div class="text-center">
+                        <h1 class="text-[16px] font-bold">Welcome back</h1>
+                        <span class="text-[32px] font-bold">Login to your account</span>
+                    </div>
+                </div>
+
+                <div class="mt-[48px]">
+                    <div class="my-3 font-bold">Email</div>
+                    <input type="text" class="border border-[#EFA969] w-[400px] py-4 pl-4" placeholder="John.snow@gmail.com">
+                </div>
+
+                <div class="">
+                    <div class="my-3 font-bold">Password</div>
+                    <input type="text" class="border border-[#EFA969] w-[400px] py-4 pl-4" placeholder="*********">
+                </div>
+
+                <div class="w-[400px] mt-[30px] font-bold text-white p-4 flex bg-[#EFA969] justify-center items-center">
+                    <button>Login now</button>
+                </div>
+
+                <div class="mt-12">
+                    <div class="flex justify-center text-[18px]">
+                        <span class="text-[#616161] mr-1">Dont have an account?</span> <a href="./index.php?dang-nhap" class="text-[#EFA969]">Join free today</a>
+                    </div>
+                </div>
+            </div>
+        </form>
+    </div>
 </div>

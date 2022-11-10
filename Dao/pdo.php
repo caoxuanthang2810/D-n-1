c54876f73a063b1fe688a992d555bd4dac60c62f
TriChua18 <trintph20275@fpt.edu.vn>
2022-11-10 07:38:52 +0700

    Add file pdo

--- Dao/pdo.php
+++ Dao/pdo.php
@@ -2,9 +2,9 @@
     //  Kết nối database
 
     function pdo_get_connect(){
-        $db_url = "mysql:host=202.92.5.49;dbname=gmkbrjnqhosting_nhom4;charset=utf8";
-        $username = "gmkbrjnqhosting_nhom4";
-        $password = "f0W9voiCVk05";
+        $db_url = "mysql:host=202.92.5.49;dbname=gmkbrjnqhosting_nhom1;charset=utf8";
+        $username = "gmkbrjnqhosting_nhom1";
+        $password = "zA1GJuGNzA8F";
         
         try {
             $connect = new PDO($db_url,$username,$password);

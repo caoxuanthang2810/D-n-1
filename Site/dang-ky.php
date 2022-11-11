8685a857cda917e5d93198bdadeaefdf44215783
TriChua18 <trintph20275@fpt.edu.vn>
2022-11-11 17:41:41 +0700

    Giao Diện Đăng ký

--- Site/dang-ky.php
+++ Site/dang-ky.php
@@ -11,21 +11,27 @@
                 <div class="mt-[48px]">
                     <div class="my-3 font-bold">Email</div>
                     <input type="text" id="email" class="border border-[#EFA969] w-[400px] py-4 pl-4 rounded-[5px]" placeholder="John.snow@gmail.com">
+                    <span id="msg-error-email" class="flex items-center mt-2 ml-2 text-[#ff4d4f]"></span>
                 </div>
 
                 <div class="">
                     <div class="my-3 font-bold">User Name</div>
                     <input type="text" id="userName" class="border border-[#EFA969] w-[400px] py-4 pl-4 rounded-[5px]" placeholder="John.snow">
+                    <span id="msg-error-username" class="flex items-center mt-2 ml-2 text-[#ff4d4f]"></span>
                 </div>
 
                 <div class="">
                     <div class="my-3 font-bold">Password</div>
                     <input type="text" id="password" class="border border-[#EFA969] w-[400px] py-4 pl-4 rounded-[5px]" placeholder="*********">
+                    <span id="msg-error-pass" class="flex items-center mt-2 ml-2 text-[#ff4d4f]"></span>
+                    <span id="msg-error-re-pass-same-1" class="flex items-center mt-2 ml-2 text-[#ff4d4f]"></span>
                 </div>
 
                 <div class="">
                     <div class="my-3 font-bold">Re-Password</div>
                     <input type="text" id="passwordcheck" class="border border-[#EFA969] w-[400px] py-4 pl-4 rounded-[5px]" placeholder="*********">
+                    <span id="msg-error-re-pass" class="flex items-center mt-2 ml-2 text-[#ff4d4f]"></span>
+                    <span id="msg-error-re-pass-same-2" class="flex items-center mt-2 ml-2 text-[#ff4d4f]"></span>
                 </div>
 
                 <div class="w-[400px] mt-[30px] font-bold p-4 flex bg-[#EFA969] justify-center items-center">

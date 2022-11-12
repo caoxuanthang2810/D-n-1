3db12dd5f07ac5237d6c0de5fc5a71be37405a12
TriChua18 <trintph20275@fpt.edu.vn>
2022-11-12 23:48:52 +0700

    Code js

--- Asset/Javascript/main.js
+++ Asset/Javascript/main.js
@@ -118,3 +118,96 @@ function checkFormLogin(){
         return false;
     }
 }
+
+function checkFormInfor(){
+    var CheckEmail = /^\w+@[a-zA-Z]{3,}\.com$/i;
+    
+    var fullname = document.getElementById("fullname");
+    var phone = document.getElementById("phone");
+    var email = document.getElementById("email");
+    var address = document.getElementById("address");
+
+    var msgErrorFullname = document.getElementById("msg-error-fullname");
+    var msgErrorPhone = document.getElementById("msg-error-phone");
+    var msgErrorEmail = document.getElementById("msg-error-email");
+    var msgErrorAddress = document.getElementById("msg-error-address");
+    
+    if(fullname.value != ""){
+        if(fullname.value.length < 3){
+            msgErrorFullname.innerHTML = "<img class='h-[10px] w-[10px] mr-1' src='../Asset/Image/logo/1200px-Nuvola_apps_error.svg.png'> Nhập họ và tên lớn hơn 3 kí tự";
+            fullname.focus();
+            return false;
+        }else{
+            alert("fwdbiqwdbwqjnkdwq")
+            msgErrorFullname.innerHTML = "";
+            fullname.focus();
+        }
+    }else{
+        msgErrorFullname.innerHTML = "<img class='h-[10px] w-[10px] mr-1' src='../Asset/Image/logo/1200px-Nuvola_apps_error.svg.png'> Nhập họ và tên";
+        fullname.focus();
+        return false;
+    }
+
+    if(phone.value != ""){
+        if(phone.value.length < 3){
+            msgErrorPhone.innerHTML = "<img class='h-[10px] w-[10px] mr-1' src='../Asset/Image/logo/1200px-Nuvola_apps_error.svg.png'> Nhập họ và tên lớn hơn 3 kí tự";
+            phone.focus();
+            return false;
+        }else{
+            alert("fwdbiqwdbwqjnkdwq")
+            msgErrorPhone.innerHTML = "";
+            phone.focus();
+        }
+    }else{
+        msgErrorPhone.innerHTML = "<img class='h-[10px] w-[10px] mr-1' src='../Asset/Image/logo/1200px-Nuvola_apps_error.svg.png'> Nhập họ và tên";
+        phone.focus();
+        return false;
+    }
+
+    if(address.value != ""){
+        if(address.value.length < 3){
+            msgErrorAddress.innerHTML = "<img class='h-[10px] w-[10px] mr-1' src='../Asset/Image/logo/1200px-Nuvola_apps_error.svg.png'> Nhập địa chỉ lớn hơn 3 kí tự";
+            address.focus();
+            return false;
+        }else{
+            msgErrorAddress.innerHTML = "";
+            address.focus();
+        }
+    }else{
+        msgErrorPass.innerHTML = "<img class='h-[10px] w-[10px] mr-1' src='../Asset/Image/logo/1200px-Nuvola_apps_error.svg.png'> Nhập địa chỉ";
+        address.focus();
+        return false;
+    }
+    
+    if(email.value != ""){
+        if(!CheckEmail.test(email.value)){
+            msgErrorEmail.innerHTML = "<img class='h-[10px] w-[10px] mr-1' src='../Asset/Image/logo/1200px-Nuvola_apps_error.svg.png'> Nhập sai định dạng email";
+            email.focus();
+            return false;
+        }else{
+            msgErrorEmail.innerHTML = "";
+            email.focus();
+        }
+        msgErrorEmail.innerHTML = "";
+        email.focus();
+    }else{
+        msgErrorEmail.innerHTML = "<img class='h-[10px] w-[10px] mr-1' src='../Asset/Image/logo/1200px-Nuvola_apps_error.svg.png'> Nhập email";
+        email.focus();
+        return false;
+    }
+
+    if(address.value != ""){
+        if(address.value.length < 3){
+            msgErrorAddress.innerHTML = "<img class='h-[10px] w-[10px] mr-1' src='../Asset/Image/logo/1200px-Nuvola_apps_error.svg.png'> Nhập địa chỉ lớn hơn 3 kí tự";
+            address.focus();
+            return false;
+        }else{
+            msgErrorAddress.innerHTML = "";
+            address.focus();
+        }
+    }else{
+        msgErrorAddress.innerHTML = "<img class='h-[10px] w-[10px] mr-1' src='../Asset/Image/logo/1200px-Nuvola_apps_error.svg.png'> Nhập địa chỉ";
+        address.focus();
+        return false;
+    }
+}

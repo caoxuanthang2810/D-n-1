5e513e0221b24ed7dcc8333e79dc275294f796ab
TriChua18 <trintph20275@fpt.edu.vn>
2022-11-12 23:48:27 +0700

    Thông tin cá nhân

---
+++ Site/thong-tin-ca-nhan-output.php
@@ -0,0 +1,40 @@
+<div class="container mx-auto">
+    <div class="flex justify-center mt-[28px]">
+        <form action="" class="w-[600px] flex justify-center mb-[190px]" method="POST">
+            <div>
+                <div class="flex justify-center">
+                    <div class="text-center">
+                        <span class="text-[32px] font-bold">Thông tin cá nhân</span>
+                    </div>
+                </div>
+
+                <div class="mt-[48px]">
+                    <div class="my-3 font-bold">Họ và tên</div>
+                    <input type="text" name="fullname" class="border border-[#EFA969] w-[400px] py-4 pl-4 rounded-[5px]" placeholder="TriChua18">
+                </div>
+
+                <div class="">
+                    <div class="my-3 font-bold">Số điện thoại</div>
+                    <input type="text" name="phone" class="border border-[#EFA969] w-[400px] py-4 pl-4 rounded-[5px]" placeholder="0385XXXXXX">
+                </div>
+
+                <div class="">
+                    <div class="my-3 font-bold">Email</div>
+                    <input type="text" name="email" class="border border-[#EFA969] w-[400px] py-4 pl-4 rounded-[5px]" placeholder="John.snow@gmail.com">
+                </div>
+
+                <div class="">
+                    <div class="my-3 font-bold">Địa chỉ</div>
+                    <input type="text" name="address" class="border border-[#EFA969] w-[400px] py-4 pl-4 rounded-[5px]" placeholder="VD: Hà nội">
+                </div>
+
+                <div class="flex justify-center">
+                    <div class="w-[150px] mt-[30px] font-bold p-4 flex bg-[#EFA969] justify-center items-center">
+                        <button>Thay đổi</button>
+                    </div>
+                </div>
+            </div>
+        </form>
+    </div>
+    <script src="../Asset/Javascript/main.js"></script>
+</div>

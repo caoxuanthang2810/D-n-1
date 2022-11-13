d541b3511d9caf3e415d85d392de706460bebe56
nguyenphucquyet <114094880+nguyenphucquyet@users.noreply.github.com>
2022-11-14 01:07:01 +0700

    up

---
+++ Site/thanh-toan.php
@@ -0,0 +1,25 @@
+<div class="container mx-auto mb-6">
+    <h2>Thanh toán</h2>
+    <div class="border border-[#EFA969] h-[200px]">
+        <div class="flex justify-center">
+            <div>
+                <img src="../Asset/Image/product/canon-eos-1500d-kit-1855mm-f3556-iii(2) 1.png" alt="" class="mx-4 my-3 w-[180px]">
+            </div>
+            <div>
+                <h3 class="font-semibold text-[21px] my-[50px] mx-7">CANON EOS 1500D KIT EF-S18-55MM F3.5-5.6 IS II</h3>
+            </div>
+        </div>
+        <div class="flex justify-center">
+            <div>
+                <p class=" mt-[-110px] font-semibold text[18px] ml-[-117px]"> Phân loại A01010220</p>
+            </div>
+            <div>
+                <p class="font-semibold text[18px] mt-[-60px] ml-[-150px]">x1</p>
+            </div>
+            
+        </div>
+        <div class="text-center  mt-[-60px] ml-[500px]">
+                <span class="font-bold text[18px] text-red-500">11,090,000 VNĐ</span>
+            </div> 
+    </div>
+</div>

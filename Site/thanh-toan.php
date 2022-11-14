0b5b62f2ed51cc16894ec8d16c44777c7f56aec5
nguyenphucquyet <114094880+nguyenphucquyet@users.noreply.github.com>
2022-11-14 19:05:46 +0700

    update

--- Site/thanh-toan.php
+++ Site/thanh-toan.php
@@ -1,5 +1,5 @@
 <div class="container mx-auto mb-6">
-    <h2>Thanh toán</h2>
+    <h2 class="font-bold text-[25px] text-center my-9">Thanh toán</h2>
     <div class="border border-[#EFA969] h-[200px]">
         <div class="flex justify-center">
             <div>
@@ -16,10 +16,48 @@
             <div>
                 <p class="font-semibold text[18px] mt-[-60px] ml-[-150px]">x1</p>
             </div>
-            
+
         </div>
         <div class="text-center  mt-[-60px] ml-[500px]">
-                <span class="font-bold text[18px] text-red-500">11,090,000 VNĐ</span>
-            </div> 
+            <span class="font-bold text[18px] text-red-500">11,090,000 VNĐ</span>
+        </div>
     </div>
+    <div class=" mt-[28px] text-center">
+        <form action="" class=" mb-[190px]  " method="POST">
+            <div class=" grid grid-cols-2">
+
+                <div class="mt-[48px]">
+                    <div class="mt-[-36px] mb-[12px] font-bold">Họ và tên</div>
+                    <input type="text" id="fullname" class="border border-[#EFA969] w-2/3 py-4 pl-4 rounded-[5px]" placeholder="TriChua18">
+                    <span id="msg-error-fullname" class="flex items-center mt-2 ml-2 text-[#ff4d4f]"></span>
+                </div>
+
+                <div class="">
+                    <div class="my-3 font-bold">Số điện thoại</div>
+                    <input type="text" id="phone" class="border border-[#EFA969] w-2/3 py-4 pl-4 rounded-[5px]" placeholder="0385XXXXXX">
+                    <span id="msg-error-phone" class="flex items-center mt-2 ml-2 text-[#ff4d4f]"></span>
+                </div>
+
+                <div class="">
+                    <div class="my-3 font-bold">Email</div>
+                    <input type="text" id="email" class="border border-[#EFA969] w-2/3 py-4 pl-4 rounded-[5px]" placeholder="John.snow@gmail.com">
+                    <span id="msg-error-email" class="flex items-center mt-2 ml-2 text-[#ff4d4f]"></span>
+                </div>
+
+                <div class="">
+                    <div class="my-3 font-bold">Địa chỉ</div>
+                    <input type="text" id="address" class="border border-[#EFA969] w-2/3 py-4 pl-4 rounded-[5px]" placeholder="VD: Hà nội">
+                    <span id="msg-error-address" class="flex items-center mt-2 ml-2 text-[#ff4d4f]"></span>
+                </div>
+
+
+            </div>
+
+        </form>
+
+        <div class="mt-[-150px]">
+            <button class=" border border-[#EFA969] w-[100px] mt-[30px] font-bold p-4 text-center hover:bg-[#EFA969] " >Mua</button>
+        </div>
+    </div>
+</div>
 </div>

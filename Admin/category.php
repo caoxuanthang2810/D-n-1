e21d570d2c6a554ad949c3ab7e5ca04428b1f22f
cuongpqph24058 <114381515+cuongpqph24058@users.noreply.github.com>
2022-11-14 17:56:19 +0700

    up

---
+++ Admin/category.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <div class=" mx-[40px]  ">
+
+        <div class="flex justify-between my-[30px]">
+            <div class="">
+                <input type="text" class="pl-2 h-[42px] rounded-[5px]" placeholder="Nhập từ khóa tìm kiếm">
+                <button class="w-[111px] h-[42px] bg-white border-2 ml-[20px] rounded-[5px]">
+                    <p>Tìm kiếm</p>
+                </button>
+            </div>
+
+            <div>
+                <button class="w-[163px] h-[50px] bg-amber-500 text-white ml-[20px] rounded-[5px]"><a href="">Thêm
+                        mới</a></button>
+            </div>
+        </div>
+
+        <div class="bg-white h-[676px]">
+            <div class="bg-slate-100 h-[50px] ">
+
+                <table>
+                    <tr class="text-center h-[55px]">
+                        <th class="w-[100px]">ID</th>
+                        <th class="w-[250px]">Tên đơn</th>
+                        <th class="w-[500px]">Giá sản phẩm</th>
+                        <th class="w-[200px]">Số lượng</th>
+                        <th class="w-[200px]">Ảnh sản phẩm</th>
+                        <th class="w-[200px]">Bảo hành</th>
+                        <th class="w-[200px]">Xuất Xứ</th>
+                        <th class="w-[200px]">Mã danh mục</th>
+                        <th class="w-[200px]">Hành động</th>
+
+                    </tr>
+
+                    <tr class="text-center h-[55px]">
+                        <td>1</td>
+                        <td>CANON EOS 1500D KIT EF-S18-55MM F3.5-5.6 IS II</td>
+                        <td>11,090,000 VNĐ</td>
+                        <td>1</td>
+                        <td><img src="../Asset/Image/items/canon-eos-1500d-kit-1855mm-f3556-iii(2) 10.png" alt="">
+                        </td>
+                        <td>24 tháng</td>
+                        <td>Đài Loan</td>
+                        <td>gà v</td>
+                        <td><button> <i class="fa-solid fa-pen mr-2"></i></button>
+                            <button><i class="fa-solid fa-trash-can"></i></button>
+                        </td>
+                    </tr>
+
+
+                </table>
+            </div>
+        </div>
+</body>
+
+</html>

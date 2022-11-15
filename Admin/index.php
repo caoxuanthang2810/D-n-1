11a4c15844740b072bab4eaebf72e8aaefa1f77f
Cao Xuân Thắng <114093877+caoxuanthang2810@users.noreply.github.com>
2022-11-15 21:05:13 +0700

    layout trang chủ & comment

--- Admin/index.php
+++ Admin/index.php
@@ -1,7 +1,11 @@
 <?php
     require_once "../Dao/pdo.php";
 
-    if(isset($_GET["category"])){
+    if(isset($_GET["trang-chu"])){
+        $VIEW_NAME = "trang-chu.php";
+        $TITLE_NAME = "Trang chủ";
+    }
+    elseif(isset($_GET["category"])){
         $VIEW_NAME = "category.php";
         $TITLE_NAME = "Trang category";
     }elseif(isset($_GET["product"])){
@@ -14,6 +18,10 @@
     elseif(isset($_GET["role"])){
         $VIEW_NAME="role.php";
         $TITLE_NAME="Trang chức vụ";
+    }
+    elseif(isset($_GET["comment"])){
+        $VIEW_NAME="comment.php";
+        $TITLE_NAME="Trang Comment";
     }
     else{
         $VIEW_NAME = "user.php";
